8572ebcbc9a7a32fcae5f3b85bb5d529a7b8afd5
shanikapaul2004 <shanikap@bu.edu>
2025-09-28 20:54:16 -0400

    everything working locally

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from .views import ProfileListView, ProfileDetailView
+
 urlpatterns = [
-    
+    path('', ProfileListView.as_view(), name='show_all_profiles'),
+    path('profile/<int:pk>', ProfileDetailView.as_view(), name='show_profile'),  # NEW
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Profile
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+class ProfileListView(ListView):
+    '''Create a subclass of ListView to display all profiles.'''
+    model = Profile
+    template_name = 'mini_insta/show_all_profiles.html'
+    context_object_name = 'profiles'
+
+class ProfileDetailView(DetailView):
+    '''Show the details for one profile.'''
+    model = Profile
+    template_name = 'mini_insta/show_profile.html'
+    context_object_name = 'profile'
